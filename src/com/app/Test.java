522ed9e1989d7d0ee045dbc3403b98379139fbb9
Swamy <klswamy225@gmail.com>
2019-10-12 14:29:48 +0530

    First file change

---
+++ src/com/app/Test.java
@@ -0,0 +1,13 @@
+package com.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hi this is test project of git");
+
+	}
+
+}
